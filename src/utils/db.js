6c50bb3b65bfedbc24635631b9d68b0479757549
Rasmus Eskola <fruitiex@gmail.com>
2017-06-06 11:31:51 +0300

    Add likeFilter helper method for easy filtering as per query string

--- src/utils/db.js
+++ src/utils/db.js
@@ -7,3 +7,34 @@ import config from './config';
 // pg.defaults.ssl = true;
 
 export default knex(config.db);
+
+/**
+ * Builds SQL query with given filter object.
+ * Returns rows matching all given fields (default), or any given field.
+ *
+ * Sample usage:
+ *
+ * knex('users')
+ *   .where(likeFilter({
+ *     name: 'foo',
+ *     email: '@bar.com'
+ *   }))
+ */
+export const likeFilter = (filters, anyField = false) => (origQuery) => {
+  let q = origQuery;
+
+  Object.keys(filters).forEach((key, index) => {
+    if (!index) {
+      // first field with .whereRaw()
+      q = q.whereRaw(`LOWER(${key}) LIKE '%' || LOWER(?) || '%'`, filters[key]);
+    } else if (anyField) {
+      // if anyField true, additional fields use .orWhereRaw() (any field must match)
+      q = q.orWhereRaw(`LOWER(${key}) LIKE '%' || LOWER(?) || '%'`, filters[key]);
+    } else {
+      // by default additional fields use .andWhereRaw() (all fields must match)
+      q = q.andWhereRaw(`LOWER(${key}) LIKE '%' || LOWER(?) || '%'`, filters[key]);
+    }
+  });
+
+  return q;
+};
