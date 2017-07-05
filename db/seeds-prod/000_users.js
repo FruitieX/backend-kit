91b1b1e383b1a88c3a0e63f3fe8279b237db1d4e
Rasmus Eskola <fruitiex@gmail.com>
2017-07-05 13:36:15 +0300

    Organize seed files according to db tables

---
+++ db/seeds-prod/000_users.js
@@ -0,0 +1,13 @@
+const fixtureFactory = require('fixture-factory');
+
+fixtureFactory.register('user', {
+  email: 'internet.email',
+  description: 'lorem.sentences',
+  scope: 'user',
+});
+
+exports.seed = knex => (
+  knex('users')
+    // Generate several test users (no password = login disabled)
+    knex.batchInsert('users', fixtureFactory.generate('user', 10))
+);
